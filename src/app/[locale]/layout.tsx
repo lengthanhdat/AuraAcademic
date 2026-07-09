fad88734116332ebd55554100743d0aa6af689a7
Le Nguyen Thanh Dat <tdat2202a@gmail.com>
2026-07-10 02:51:48 +0700

    fix: correct Google Fonts URL syntax

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -35,11 +35,11 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           rel="preload"
-          href="https://fonts.googleapis.com/css2/?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           as="style"
         />
         <link
-          href="https://fonts.googleapis.com/css2/?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
       </head>
